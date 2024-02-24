351d3c7e080873e87b9b14e5f50b68b359149a09
obaidTariq5 <obaid@geekgenix.com>
2024-02-24 09:55:09 +0500

    add a file of eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // Other ESLint configurations...
+    rules: {
+      // Other rules...
+      // Disable the 'no-unused-vars' rule
+      'no-unused-vars': 'off',
+    },
+  };
